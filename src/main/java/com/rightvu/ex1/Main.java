8cb5b353ee14f783923da8b60b2f789473f1eb30
Malik <malik.ismail@gmail.com>
2022-05-01 09:45:46 +0800

    Added MainProgram

---
+++ src/main/java/com/rightvu/ex1/Main.java
@@ -0,0 +1,10 @@
+package com.rightvu.ex1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
